5f568cf1cf47c88ba2d7230a3f97cc1fd837e6d4
Irina Scurtu <scurtuirina@gmail.com>
2024-04-01 15:46:13 +0300

    encapsulations

---
+++ OOP/Encapsulation/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Person
+    {
+        private readonly string cnp;
+        private readonly string firstName;
+        private string lastName;
+        // private decimal salary;
+
+
+        public Person(string cnp, string firstName, string lastName)
+        {
+            this.cnp = cnp;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            //this.salary = 10000;
+            this.Salary = 10000;
+        }
+
+        public string CNP
+        {
+            get
+            {
+                return cnp;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+        }
+
+        // public decimal Salary { get { return salary; } }
+
+        public decimal Salary { get; private set; }
+
+        public void GetMarried(string newName)
+        {
+            this.lastName = newName;
+            this.IncreaseSalary();
+        }
+
+        public void IncreaseSalary()
+        {
+            // this.salary = this.salary * (decimal)1.3;
+            this.Salary =  Salary * (decimal)1.3;
+        }
+    }
+}
---
+++ OOP/Encapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("32323232", "Gigi", "Davidescu");
+            Console.WriteLine(person.CNP);
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+            // person.LastName = "dsadsa"; not possible
+            Console.WriteLine(person.Salary);
+            person.GetMarried("Testarescu");
+            Console.WriteLine(person.LastName);
+            Console.WriteLine(person.Salary);
+
+            //person.Salary = 1010101;
+            person.IncreaseSalary();
+            person.IncreaseSalary();
+            person.IncreaseSalary();
+            person.IncreaseSalary();
+            person.IncreaseSalary();
+            Console.WriteLine(person.Salary);
+
+        }
+    }
+}
