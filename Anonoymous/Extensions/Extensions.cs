ac86daad29fb5ed8952eedd70b71822be9221873
Irina Scurtu <scurtuirina@gmail.com>
2024-04-25 10:15:59 +0300

    linq

---
+++ Anonoymous/Extensions/Extensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static int WordCount(string myString)
+        {
+            //"ana has apples"
+            string[] words = myString.Split(' ');
+            int noOfWords = words.Length;
+            return noOfWords;
+        }
+
+        public static int WordCountExtension(this string myString)
+        {
+            int noOfWords = myString.Split(' ').Length;
+            return noOfWords;
+        }
+
+        public static bool GreaterThan(this int a, int b)
+        {
+            return a > b;//a.GreaterThan(b)
+        }
+
+
+        public static DateTime RoundToHalfHour(this DateTime input)
+        {
+            //14:01- 14:29 -> 14:00
+            //14:30- 14:59 -> 15:00
+            DateTime dateToReturn = new DateTime(input.Year, input.Month, input.Day, input.Hour, input.Minute, 0);
+            if (input.Minute == 0)
+            {
+                return input;
+
+            }
+
+            if (input.Minute >= 1 && input.Minute < 30)
+            {
+                return dateToReturn.AddMinutes(30 - input.Minute);
+            }
+            //else
+            //{
+            return dateToReturn.AddMinutes(60 - input.Minute);
+            //}
+
+
+            // return dateToReturn;
+        }
+
+    }
+}
---
+++ Anonoymous/Extensions/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = "Anna has apples";
+            var wordNo = Extensions.WordCount(myString);
+            int wordNoFromExtension = myString.WordCountExtension();
+
+            int a = 0;
+            int b = 10;
+            if (a.GreaterThan(b))
+            {
+                Console.WriteLine("A");
+            }
+
+            var myTime = DateTime.Now;
+            Console.WriteLine(myTime.RoundToHalfHour());
+            myTime = new DateTime(myTime.Year, myTime.Month, myTime.Day, 15, 45, 00);
+            Console.WriteLine(myTime.RoundToHalfHour());
+
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //count all odd numbers
+
+            var count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 1)
+                {
+                    count++;
+                    //count = count + 1;
+                }
+
+            }
+            Console.WriteLine($"The number of odd numbers is: {count}");
+
+            var oddNumbers = numbers.Count(x => x % 2 == 1);
+            var greaterThan6 = numbers.Count(item => item >= 6);
+
+            Func<int, bool> intFn = (x) => x < 10;
+
+            var lessThanTen = numbers.Count(x => intFn(x));
+
+            List<Person> people = new List<Person>()
+            {
+                new Person { FirstName = "John", LastName = "Doe", City = "New York", Country = "USA" },
+                new Person { FirstName = "Jane", LastName = "Smith", City = "Los Angeles", Country = "USA" },
+                new Person { FirstName = "David", LastName = "Brown", City = "London", Country = "UK" },
+                new Person { FirstName = "Emily", LastName = "Johnson", City = "Toronto", Country = "Canada" },
+                new Person { FirstName = "Michael", LastName = "Lee", City = "Sydney", Country = "Australia" }
+            };
+
+            var peopleNoInUsa = people.Count(x => x.Country == "USA");
+
+            var peopleInTwoWordsCity = people.Where(x => x.City.WordCountExtension() >= 2).ToList();
+
+            var orderedByCountry = people
+                .OrderBy(x => x.Country)
+                .OrderByDescending(x => x.City)
+                .ToList();
+        }
+    }
+}
